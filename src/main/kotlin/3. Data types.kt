8989182e0f431da11f546484abf3e5c216fe04a0
Rohit Balage <thatcomputerboy26@gmail.com>
2024-02-04 22:25:09 -0500

    added 3. data types

---
+++ src/main/kotlin/3. Data types.kt
@@ -0,0 +1,33 @@
+fun main()
+{
+    var variablename = "rohit"
+
+
+    // Byte : -128 to 127
+
+    val b : Byte = 100
+
+    println(b)
+
+    // var b: Byte = 300  will give an error.
+
+
+    // Short: -32786  to 32767
+
+    val s : Short = 7000
+
+    println(s)
+
+    // Integer : -2147483648 to 2147483647
+
+    val i : Int  = 99999
+
+    println(i)
+
+    // Long: -9223372036854775807  to 922337203685477580
+
+    val l:  Long = 73444489L
+
+    println(l)
+
+}
