28e1f6728c97fc6fb57395a3979060378e80cdd2
Rohit Balage <thatcomputerboy26@gmail.com>
2024-02-11 20:01:54 -0500

    added function passing oop

---
+++ src/main/kotlin/Kotlin OOP/1. passing parameters.kt
@@ -0,0 +1,14 @@
+
+fun main()
+{
+
+    myFuntion("Jack", 26)
+}
+
+
+// arguments must passed in the same order  and with same data type
+
+fun myFuntion(fname : String, age : Int)
+{
+    println("hello my friend "+fname + " and your name is " +age)
+}
