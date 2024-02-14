59712614f3826d3f93d0cc7bff99273580adaf8c
Rohit Balage <thatcomputerboy26@gmail.com>
2024-02-13 21:05:19 -0500

    added Kotlin DSA 1. Collection Overview

---
+++ src/main/Kotin Data Structures/collection overview.kt
@@ -0,0 +1,18 @@
+/*
+* Collection: ususally contains a number of objects of the same type
+* and these objects in the collection are called elements or items
+*
+* Kotlin standard library provides a rich set of tools for
+* managing collections
+*
+*
+*
+*
+* Type of collections:
+*
+* In kotlin collections are categories into two forms
+*
+* Immutable collection  --- cannot changed
+*Mutable collection  ---- changed
+*
+* */
