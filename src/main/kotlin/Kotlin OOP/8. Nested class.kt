d120a807187101f12c015eb079e7c3f900477396
Rohit Balage <thatcomputerboy26@gmail.com>
2024-02-13 20:57:16 -0500

    added nested class

---
+++ src/main/kotlin/Kotlin OOP/8. Nested class.kt
@@ -0,0 +1,19 @@
+package `Kotlin OOP`
+
+
+fun main()
+{
+    var o1 = Outer()
+    o1.age
+
+
+
+
+    // this is how you access the nested class properties:
+
+
+    var nested = Outer.Nested()
+    nested.length
+
+
+}
---
+++ src/main/kotlin/Kotlin OOP/Outer.kt
@@ -0,0 +1,21 @@
+package `Kotlin OOP`
+
+class Outer {
+    // this is an example nested class
+
+    var age = 25
+
+
+
+
+    // Nested class
+
+    class Nested{
+
+        //..................
+
+        var length = 15
+
+    }
+
+}
