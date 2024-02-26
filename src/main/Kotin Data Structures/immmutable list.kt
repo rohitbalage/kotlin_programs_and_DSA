a0bcf7f35b9eb8da07c254730e343aaef8244143
Rohit Balage <thatcomputerboy26@gmail.com>
2024-02-26 14:28:17 -0500

    added immutable list

---
+++ src/main/Kotin Data Structures/immmutable list.kt
@@ -0,0 +1,23 @@
+
+
+ fun main()
+
+{
+
+
+ // imutable list
+
+    var list1 = listOf<Int>(4,5,6)
+
+    var list2 = listof("Jack", "John", "Mike")
+
+    // list1.add : will give an error.
+
+    // printing
+
+    for(item in list1)
+    {
+        println(item)
+    }
+
+}
