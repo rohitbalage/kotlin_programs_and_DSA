c055eb72803fbf7a32f5ee31cfff1838436b770b
Rohit Balage <thatcomputerboy26@gmail.com>
2024-02-26 14:38:05 -0500

    added immutable set

---
+++ src/main/kotlin/Kotin Data Structures/immutable set.kt
@@ -0,0 +1,20 @@
+
+
+
+
+fun main()
+{
+
+    var immutableSet = setOf(5,6,"Hello", 6,0,0,5)
+
+
+    // sets used when we need to deal with unique elements
+
+    // print only unique elements
+    for(item in immutableSet)
+    {
+        println(item)
+    }
+
+
+}
