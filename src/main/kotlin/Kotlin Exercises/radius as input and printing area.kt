110132d0d43a1061f756fff25309ee9320426702
Rohit Balage <thatcomputerboy26@gmail.com>
2024-02-27 17:05:29 -0500

    added exercise  2. radius as input and printing areas as output

---
+++ src/main/kotlin/Kotlin Exercises/radius as input and printing area.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main()
+{
+  var s1 = Scanner(System.`in`)
+
+    println("Please enter radius")
+    var radius = s1.nextInt()
+
+
+    var area = radius * 3.14 * 2
+
+    println("the area is $area")
+}
