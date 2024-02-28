bcbe52002b6b707127c1063c18885a66c484cb78
Rohit Balage <thatcomputerboy26@gmail.com>
2024-02-28 09:58:13 -0500

    added 5. higer order functions

---
+++ src/main/kotlin/Kotlin lamda expressions and higher order functions/5. higher order functions.kt
@@ -0,0 +1,29 @@
+/*
+* Higher order functions:
+*
+* It a function that takes a function as parameter or return a function or both
+*
+*
+* In general, lamda expression are passed as an argument to a higher-order function or returned
+* argument to a higher-order function or returned from it. we can also use anonymous functions for
+* the same.
+*
+*
+* */
+
+
+fun main()
+{
+
+    val add = {a: Int, b: Int  -> a+b}
+    hof(add)  // calling higher order function
+}
+
+// Higher order functions
+
+fun hof(addition: (Int, Int) -> Int)
+{
+    val result = addition(4,5)
+    println(result)
+
+}
