b5577a171000c5d401a51762eba931fe6bbe2ff9
Rohit Balage <thatcomputerboy26@gmail.com>
2024-02-28 10:21:17 -0500

    adding it example

---
+++ src/main/kotlin/Kotlin lamda expressions and higher order functions/6. It keyword.kt
@@ -0,0 +1,39 @@
+fun main()
+{
+
+hof3 { println("Hi $it") }
+
+
+    val numbers = listOf(1,2,3,4,5)
+    println(numbers.filter { numbers -> numbers >3 })
+
+
+    // using with it
+    println(numbers.filter { it >3 })
+
+
+
+    //
+
+    reverseString("Hello" ){s -> s.reversed() }  // lamda is the last parameter of paranthessis then we can
+    // move it out of paranthesis
+
+    // using it
+    reverseString("Hello" ){it.reversed() }
+
+}
+
+fun hof3(name : (String) -> Unit)
+{
+    name("John")
+}
+
+fun reverseString(str: String, myLamda: (String)->(String))
+{
+    var result = myLamda(str)
+
+    println(result )
+}
+
+
+
